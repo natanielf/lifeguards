2f546b07f387f5440216361eb41fce8691ac66cb
Nataniel Farzan <natanielfarzan@gmail.com>
2024-07-03 10:43:05 -0700

    Add assertions to validate input, simplify solution

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 // A solution to Lifeguards: USACO 2018 January Contest, Bronze (Problem 2)
 
+#include <assert.h>
+
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -13,7 +15,7 @@ const string OUTPUT_FILE_NAME = "lifeguards.out";
 // Constraints given by the prompt
 const unsigned int MIN_TIME = 0;
 const unsigned int MAX_TIME = 1000;
-const unsigned int NUM_SHIFTS = (MAX_TIME - MIN_TIME) + 1;
+const unsigned int NUM_SHIFTS = MAX_TIME - MIN_TIME;
 
 // Store start and end times for a lifeguard shift
 typedef struct lifeguard {
@@ -40,6 +42,7 @@ int main(int argc, char **argv) {
     // Read the number of lifguards
     unsigned int num_lifeguards = 0;
     input_file >> num_lifeguards;
+    assert(num_lifeguards >= 1 && num_lifeguards <= 1000);
 
     // Declare lifeguard array
     lifeguard_t lifeguards[num_lifeguards];
@@ -48,48 +51,53 @@ int main(int argc, char **argv) {
     for (unsigned int i = 0; i < num_lifeguards; i++) {
         unsigned int start_time = 0;
         input_file >> start_time;
+        assert(start_time >= 0 && start_time <= 1000);
         unsigned int end_time = 0;
         input_file >> end_time;
+        assert(end_time >= 0 && end_time <= 1000);
         lifeguards[i] = lifeguard_t{start_time, end_time};
     }
 
     // Close the input file
     input_file.close();
 
-    // Store the number of lifeguards on duty
-    unsigned int lifeguards_on_duty[num_lifeguards][NUM_SHIFTS];
-    // Initialize the number of lifeguards on duty to 0
+    // Store the number of lifeguards on duty at any given point in time
+    unsigned int lifeguards_on_duty[NUM_SHIFTS];
+    // Initialize the number of lifeguards on duty at all points in time to 0
+    for (unsigned int i = 0; i < NUM_SHIFTS; i++) {
+        lifeguards_on_duty[i] = 0;
+    }
+
+    // Populate the number of lifeguards on duty
     for (unsigned int i = 0; i < num_lifeguards; i++) {
-        for (unsigned int j = 0; j < NUM_SHIFTS; j++) {
-            lifeguards_on_duty[i][j] = 0;
+        lifeguard_t lifeguard = lifeguards[i];
+        for (unsigned int j = lifeguard.start; j < lifeguard.end; j++) {
+            lifeguards_on_duty[j]++;
         }
     }
 
-    // Determine how many lifeguards are on duty at any given point in time
+    // Determine the maximum amount of time that can be covered by the remaining
+    // lifeguards after firing exactly one lifeguard
+    unsigned int max_time = 0;
     for (unsigned int i = 0; i < num_lifeguards; i++) {
-        for (unsigned int j = 0; j < num_lifeguards; j++) {
-            if (i == j) continue;
-            for (unsigned int k = 0; k < NUM_SHIFTS; k++) {
-                if (k >= lifeguards[j].start && k < lifeguards[j].end) {
-                    lifeguards_on_duty[i][k]++;
-                }
-            }
-        }
-    }
-
-    // Determine the longest continuous interval of time that can be covered
-    // after firing one lifeguard
-    unsigned int max_interval = 0;
-    for (unsigned int i = 0; i < num_lifeguards; i++) {
+        // Choose which lifeguard will be fired
+        lifeguard_t fired_lifeguard = lifeguards[i];
         unsigned int curr_max = 0;
-        for (unsigned int j = 0; j < NUM_SHIFTS; j++) {
-            while (lifeguards_on_duty[i][j] >= 1 && j < NUM_SHIFTS) {
+        for (unsigned int k = 0; k < NUM_SHIFTS; k++) {
+            unsigned int num_on_duty = lifeguards_on_duty[k];
+            // Decrement the number of lifeguards on duty if the current shift
+            // will be affected by the fired lifeguard
+            if (k >= fired_lifeguard.start && k < fired_lifeguard.end) {
+                num_on_duty--;
+            }
+            // Increment the amount of time covered
+            if (num_on_duty >= 1) {
                 curr_max++;
-                j++;
             }
-            if (curr_max > max_interval) {
-                max_interval = curr_max;
-            }
+        }
+        // Update the maximum amount of time that can be covered
+        if (curr_max > max_time) {
+            max_time = curr_max;
         }
     }
 
@@ -103,7 +111,7 @@ int main(int argc, char **argv) {
     }
 
     // Write to the output file
-    output_file << max_interval << endl;
+    output_file << max_time << endl;
 
     // Close the output file
     output_file.close();
